54bb299d0aac229db632aea1a776d0bc7957a0c7
khuongsatou <yoshi240499@gmail.com>
2019-11-08 16:34:10 +0700

    version 1.2 update normal

--- app/src/main/java/com/nvk/responsivelayout/MainActivity.java
+++ app/src/main/java/com/nvk/responsivelayout/MainActivity.java
@@ -2,13 +2,24 @@ package com.nvk.responsivelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn12 = findViewById(R.id.button12);
+        btn12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),normalActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nvk/responsivelayout/normalActivity.java
@@ -0,0 +1,14 @@
+package com.nvk.responsivelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class normalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal);
+    }
+}
